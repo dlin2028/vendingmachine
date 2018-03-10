833b0f3bf297bbe919d53102304d3ca5d39a9b6a
David L <dlin2028@gmail.com>
2018-03-10 12:37:23 -0800

    rlection

--- Vendingmachine/Program.cs
+++ Vendingmachine/Program.cs
@@ -1,23 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Vendingmachine
-{
-    class Program
+{    
+    public class CoinValueAttribute : Attribute
     {
-        static void GiveChange(ref int cents, ref Dictionary<int, int> coins)
-        {
-            List<int> coinTypes = new List<int>(coins.Keys);
+        public int Value { get; set; }
 
-            foreach (int type in coinTypes)
+        public CoinValueAttribute(int value)
+        {
+            Value = value;
+        }
+    }
+
+    class ExampleVendingMachine
+    {
+        public enum CoinTypes
+        {
+            [CoinValue(25)]
+            Quarter = 1,
+
+            [CoinValue(10)]
+            Dime,
+
+            [CoinValue(5)]
+            Nickel,
+
+            [CoinValue(1)]
+            Penny
+        }
+
+        private int GetCoinValue(CoinTypes coin)
+        {
+            Type coinType = typeof(CoinTypes);
+            FieldInfo coinInType = coinType.GetField(coin.ToString());
+            var coinValueAttribute = coinInType.GetCustomAttribute<CoinValueAttribute>(false);
+            //hi
+            return coinValueAttribute.Value;
+        }
+
+        private SortedDictionary<CoinTypes, int> CoinInventory = new SortedDictionary<CoinTypes, int>()
+        {
+            { CoinTypes.Dime, 100 },
+            { CoinTypes.Quarter, 100 },
+            { CoinTypes.Nickel, 0 },
+            { CoinTypes.Penny, 2 }
+        };
+
+        public void GetCoinsFromCustmer()
+        {
+            foreach (var item in CoinInventory)
             {
-                Console.WriteLine($"here are {coins[type].ToString()} {type} cent coins");
-                cents -= coins[type] * type;
+                Console.WriteLine($"{(int)item.Key}: {item.Key.ToString()}");
             }
 
+            //TODO: Get user input
+            int userInput = 3;
+
+            if (userInput < 1 || userInput > 4)
+            {
+                //TODO: Handle Invalid entry...
+            }
+
+            CoinTypes coinIn = (CoinTypes)userInput;
+            int value = GetCoinValue(coinIn);
+        }
+    }
+
+
+
+
+
+    class Program
+    {
+        struct Item
+        {
+            public Item(int price, int stock)
+            {
+                this.price = price;
+                this.stock = stock;
+            }
+            public int price;
+            public int stock;
+        }
+        static void GiveChange(ref int cents)
+        {
             Console.WriteLine($"here are {cents / 25} quarters");
             cents = cents % 25;
             Console.WriteLine($"here are {cents / 10} dimes");
@@ -25,122 +96,150 @@ namespace Vendingmachine
             Console.WriteLine($"here are {cents / 5} nickels");
             cents = cents % 5;
             Console.WriteLine($"here are {cents} pennies");
-
+            cents = 0;
         }
-        static void SubractChange(ref int cents, int maxCentsToRemove, ref Dictionary<int, int> coins)
-        {
-            List<int> coinTypes = new List<int>(coins.Keys);
-            coinTypes.Sort();
-
-            for (int i = coinTypes.Count - 1; i >= 0; i--)
-            {
-                int type = coinTypes[i];
-                if (type > maxCentsToRemove)
-                {
-                    continue;
-                }
-
-                while (type < maxCentsToRemove && coins[type] > 0)
-                {
-                    coins[type]--;
-                    maxCentsToRemove -= type;
-                    cents -= type;
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            Dictionary<int, int> coins = new Dictionary<int, int>();
+            var example = new ExampleVendingMachine();
+            example.GetCoinsFromCustmer();
+
+
+
+
             int cents = 0;
-            int value = int.MaxValue;
+            List<int> acceptedBills = new List<int>();
+            Dictionary<string, int> coins = new Dictionary<string, int>();
+            Dictionary<string, int> coinValues = new Dictionary<string, int>();
 
-            Random rnd = new Random();
+            #region money
+            acceptedBills.Add(1);
+            acceptedBills.Add(2);
+            acceptedBills.Add(5);
+            acceptedBills.Add(10);
+            acceptedBills.Add(20);
+            acceptedBills.Add(50);
+            acceptedBills.Add(100);
 
-            //I really like while true loops
-            while (true) //while machine not under attack/dead
+            coinValues.Add("quarter", 25);
+            coinValues.Add("nickle", 5);
+            coinValues.Add("dime", 10);
+            coinValues.Add("penny", 1);
+
+            coins.Add("quarter", 0);
+            coins.Add("nickle", 0);
+            coins.Add("dime", 0);
+            coins.Add("penny", 0);
+            #endregion
+
+            List<string> types = new List<string>(coins.Keys);
+
+            Dictionary<string, Item> items = new Dictionary<string, Item>();
+
+            items.Add("issac", new Item(674, 5));
+            items.Add("alex", new Item(100, 5));
+            items.Add("stan", new Item(1, 5));
+            items.Add("thad", new Item(312, 5));
+
+            List<string> itemNames = new List<string>(items.Keys);
+
+            Random rng = new Random();
+
+            Console.WriteLine("Type 0 to move on, type -1 to give change");
+
+            int input = 0;
+            while (true)
             {
-                Console.WriteLine("BTW: write 0 to move on, -1 to get change");
-                while (true) //while value != 0
+                Console.WriteLine("Insert bills");
+
+                while (true)
                 {
-                    Console.WriteLine("insert bills");
-
-                    value = int.Parse(Console.ReadLine());
-
-                    if (value == 0) break;
-                    else if (value == -1)
+                    input = int.Parse(Console.ReadLine());
+                    if (input == 0)
                     {
-                        GiveChange(ref cents, ref coins);
+                        break;
+                    }
+                    else if (input == -1)
+                    {
+                        GiveChange(ref cents);
+                        Console.WriteLine("Insert bills");
                         continue;
                     }
-
-                    cents += value * 100;
-
-                    Console.WriteLine("total: $" + string.Format("{0:#.00}", Convert.ToDecimal(cents.ToString()) / 100));
+                    else if (!acceptedBills.Contains(input))
+                    {
+                        Console.WriteLine($"thanks for the fake bill");
+                        continue;
+                    }
+                    Console.WriteLine($"Inserted ${input} bill");
+                    cents += input * 100;
+                    Console.WriteLine("Total: $" + string.Format("{0:#.00}", Convert.ToDecimal(cents.ToString()) / 100));
                 }
 
-                while (true) //while value != 0
+
+                Console.WriteLine("Insert coins");
+                for (int i = 0; i < coins.Keys.Count; i++)
                 {
-                    Console.WriteLine("insert value of coin");
-                    Console.WriteLine("25 = quarter");
-                    Console.WriteLine("10 = dime");
-                    Console.WriteLine("5 = nickel");
-                    Console.WriteLine("1 = penny");
+                    Console.WriteLine($"{i + 1} for {coins.Keys.ElementAt(i)}");
+                }
 
-                    value = int.Parse(Console.ReadLine());
-
-                    if (value == 0) break;
-                    else if (value == -1)
+                while (true)
+                {
+                    var help = Console.ReadKey().KeyChar.ToString();
+                    input = int.Parse(help);
+                    if (input == 0)
                     {
-                        GiveChange(ref cents, ref coins);
-                        continue;
+                        break;
+                    }
+                    else if (input == -1)
+                    {
+                        GiveChange(ref cents);
+                        break;
+                    }
+                    input--;
+                    Console.WriteLine($"Inserted {types[input]}");
+                    cents += coinValues[types[input]];
+                    Console.WriteLine("Total: $" + string.Format("{0:#.00}", Convert.ToDecimal(cents.ToString()) / 100));
+                }
+
+
+                Console.WriteLine("Items:");
+                List<string> ItemNames = new List<string>(items.Keys);
+                for (int i = 0; i < itemNames.Count; i++)
+                {
+                    Console.WriteLine($"Press {i} for {itemNames[i]}  in stock: {items[itemNames[i]].stock}, price: {items[itemNames[i]].price}");
+                }
+                while (true)
+                {
+                    input = int.Parse(Console.ReadKey().ToString());
+                    if (input == 0)
+                    {
+                        break;
+                    }
+                    else if (input == -1)
+                    {
+                        GiveChange(ref cents);
+                        break;
                     }
 
-                    if (coins.ContainsKey(value))
+                    if (items[itemNames[input]].stock <= 0)
                     {
-                        coins[value]++;
+                        Console.WriteLine("out of stock haha thanks for the $$$");
+                    }
+                    else if (rng.Next(0, 5) == 0)
+                    {
+                        Console.WriteLine($"haha your {itemNames[input]} got stuck ");
                     }
                     else
                     {
-                        coins.Add(value, 1);
+                        Console.WriteLine($"enjoy your {itemNames[input]}");
                     }
 
-                    cents += value;
-                    Console.WriteLine("total: $" + string.Format("{0:#.00}", Convert.ToDecimal(cents.ToString()) / 100));
+                    input--;
+                    cents -= items[itemNames[input]].price;
+                    Console.WriteLine("Total: $" + string.Format("{0:#.00}", Convert.ToDecimal(cents.ToString()) / 100));
                 }
 
-                while(true) //while value != 0
-                {
-                    Console.WriteLine("what is the cost of the thing you wanna buy in cents");
-
-                    value = int.Parse(Console.ReadLine());
-
-                    if (value == 0) break;
-                    else if (value == -1)
-                    {
-                        GiveChange(ref cents, ref coins);
-                        continue;
-                    }
-
-                    if (value > cents)
-                    {
-                        Console.WriteLine("not enough money");
-                        continue;
-                    }
-                    
-                    if(rnd.Next(0,2) == 0)
-                    {
-                        Console.WriteLine("haha it got stuck sucks for you");
-                    }
-                    else
-                    {
-                        Console.WriteLine("thanks for the $$$");
-                    }
-                    cents -= value;
-                    GiveChange(ref cents, ref coins);
-                    break;
-                }
             }
+
         }
     }
 }
